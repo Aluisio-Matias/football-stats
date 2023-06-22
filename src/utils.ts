67e4f630e8f5ce9009fe35f2a68613188b1ec5aa
Aluisio-Matias <aluisiomatias1@gmail.com>
2023-06-21 23:36:47 -0700

    created utils.ts file, with a function to convert string dates to number dates

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export const dataStringToDate = (dateString: string): Date => {
+  const dateParts = dateString
+    .split('/')
+    .map((value: string): number => {
+      return parseInt(value);
+  });
+
+  //changing the date structure from CSV file to Date object
+  //returning year / month (0 indexed = Jan === 0 and so on) / then the day
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
