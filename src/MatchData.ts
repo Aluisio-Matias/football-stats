c506df2d5051366dce3f04e0c2cef5fc3d84233d
Aluisio-Matias <aluisiomatias1@gmail.com>
2023-06-26 13:16:05 -0700

    Began refactoring code to  a composition-based approach

---
+++ src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchResult } from "./MatchResult";
+
+//defining a data tuple
+export type MatchData = [
+  Date, 
+  string, 
+  string, 
+  number, 
+  number, 
+  MatchResult, 
+  string
+];
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv')
 
-console.log(reader.data);
+/**
+ * Create an instance of MatchReader and pass in something satifying
+ * the 'DataReader' interface
+ */
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load(); //now anywhere below you can reference matchReader.matches
+console.log(matchReader.matches);
 
 let manUnitedWins = 0;
 //iteration to check number of wins for Manchester United at home and away games
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
